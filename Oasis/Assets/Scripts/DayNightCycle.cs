35ed416e924de67e78f25f0349e5ad35caed21b0
Zach <spywoawe@gmail.com>
2020-05-14 10:12:25 +1200

    Day/Night Cycle

---
+++ Oasis/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public GameObject dayNight;
+    public float rotateSpeed = 1f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dayNight.transform.rotation = Quaternion.Euler(1f, 0f, 0f);
+        dayNight.transform.Rotate(rotateSpeed * Time.time, 0f, 0f);
+
+        Debug.Log(Time.time);
+    }
+}
