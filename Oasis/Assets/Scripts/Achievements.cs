fd2d1e50d08031025e475972688a30b9471b8de4
Unknown <robert.a.nickel@gmail.com>
2020-05-13 12:30:31 +1200

    Actually Achievements now
    Saved scene with UI elements for achievements

--- Oasis/Assets/Scripts/Achievements.cs
+++ Oasis/Assets/Scripts/Achievements.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Achivements : MonoBehaviour
+public class Achievements : MonoBehaviour
 {
 
     //Universal Variables
