37f9ddb0423ff122099086fc2eed03a5a428019a
Zach <spywoawe@gmail.com>
2020-05-14 12:51:04 +1200

    Return to Title

--- Oasis/Assets/Scripts/TitleScreen.cs
+++ Oasis/Assets/Scripts/TitleScreen.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
