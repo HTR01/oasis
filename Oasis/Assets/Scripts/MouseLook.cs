a251e30b74a1c0c071aa1c2bf74a4241653428e6
Zach <spywoawe@gmail.com>
2020-05-18 13:17:48 +1200

    Controller Support

--- Oasis/Assets/Scripts/MouseLook.cs
+++ Oasis/Assets/Scripts/MouseLook.cs
@@ -23,11 +23,16 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        float controllerX = Input.GetAxisRaw("Controller X") * mouseSensitivity * Time.deltaTime;
+        float controllerY = Input.GetAxisRaw("Controller Y") * mouseSensitivity * Time.deltaTime;
+
         xRotation -= mouseY;
+        xRotation -= controllerY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+        playerBody.Rotate(Vector3.up * controllerX);
 
         Ahead = this.gameObject.transform.forward;
         Side = this.gameObject.transform.right;
