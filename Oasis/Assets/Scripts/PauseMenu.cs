186750d083f887c17dfda237f3903e6edaa0c98d
Zach <spywoawe@gmail.com>
2020-05-18 13:49:13 +1200

    Updated Controller Support

--- Oasis/Assets/Scripts/PauseMenu.cs
+++ Oasis/Assets/Scripts/PauseMenu.cs
@@ -10,7 +10,7 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetButtonDown("Pause"))
         {
             pause.SetActive(true);
             Time.timeScale = 0;
