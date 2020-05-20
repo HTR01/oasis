44073dbb14b7021ece299b52b2cf4552a56c0765
Zach <spywoawe@gmail.com>
2020-05-21 09:03:32 +1200

    GitIgnore for screenshots + Optimised saving

--- Oasis/Assets/Scripts/TakeScreenshot.cs
+++ Oasis/Assets/Scripts/TakeScreenshot.cs
@@ -1,20 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Security.Cryptography;
 using System.Timers;
 using UnityEngine;
+using UnityEditor;
 
 public class TakeScreenshot : MonoBehaviour
 {
-    int random;
-    
+    string m_Path;
+
+    private void Start()
+    {
+        m_Path = Application.dataPath + "/Screenshots/";        
+    }
 
     void Update()
     {
+        string date = System.DateTime.Now.ToString();
+        date = date.Replace("/", "-");
+        date = date.Replace(" ", "_");
+        date = date.Replace(":", "-");
+
         if (Input.GetButtonDown("Screenshot"))
         {
-            ScreenCapture.CaptureScreenshot("Screenshot" + Time.time + ".jpg");
+            ScreenCapture.CaptureScreenshot(m_Path + "Screenshot" + date + ".jpg");
         }
     }
 }
