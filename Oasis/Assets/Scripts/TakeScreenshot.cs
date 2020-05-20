eee7ab058c4abe74316950b9de0a82d08d8c1bd2
Zach <spywoawe@gmail.com>
2020-05-20 13:23:38 +1200

    Take ingame screenshot on button press

---
+++ Oasis/Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Security.Cryptography;
+using System.Timers;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour
+{
+    int random;
+    
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Screenshot"))
+        {
+            ScreenCapture.CaptureScreenshot("Screenshot" + Time.time + ".jpg");
+        }
+    }
+}
