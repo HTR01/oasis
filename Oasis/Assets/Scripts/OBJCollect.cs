b8af5d1596c23ed5ad3460b4ca8acbb9ac3b3aeb
Zach <spywoawe@gmail.com>
2020-05-27 09:51:40 +1200

    Added worldV3

--- Oasis/Assets/Scripts/OBJCollect.cs
+++ Oasis/Assets/Scripts/OBJCollect.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GameAnalyticsSDK;
+using System.IO;
 
 public class OBJCollect : MonoBehaviour
 {
-   
+    string m_Path;
+
+    void Start()
+    {
+        m_Path = Application.dataPath + "/Screenshots/Achievements/";
+
+        if (!Directory.Exists(m_Path))
+        {
+            System.IO.Directory.CreateDirectory(m_Path);
+        }
+    }
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Collectable")
@@ -16,6 +28,7 @@ public class OBJCollect : MonoBehaviour
                 GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "Tutorial");
                 Destroy(col.gameObject);
                 Achievements.OBJCollected = true;
+                ScreenCapture.CaptureScreenshot(m_Path + "Achievement1" + ".jpg");
             }
         }
     }
