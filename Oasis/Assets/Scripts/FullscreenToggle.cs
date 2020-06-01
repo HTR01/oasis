11cd5cbe5e3fb1725f8d9ac1cef7560812a1c1d9
Zach <spywoawe@gmail.com>
2020-06-02 09:56:34 +1200

    Fullscreen toggle + resolution dropdown

---
+++ Oasis/Assets/Scripts/FullscreenToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullscreenToggle : MonoBehaviour
+{
+    public bool fullscreen = true;
+    public Toggle fullscreenCheck;
+
+    void Update()
+    {
+        if (fullscreenCheck.isOn == true)
+        {
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            Screen.fullScreen = false;
+        }
+    }
+}
