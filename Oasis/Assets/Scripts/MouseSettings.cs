3d6a2773cc48cf8a4993df633a7120db5787542b
Zach <spywoawe@gmail.com>
2020-06-04 09:26:23 +1200

    Mouse Sensitivity

---
+++ Oasis/Assets/Scripts/MouseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseSettings : MonoBehaviour
+{
+    public MouseLook mouseL;
+
+    public void MouseChange(float mouseValue)
+    {
+        mouseL.mouseSensitivity = mouseValue;
+    }
+}
