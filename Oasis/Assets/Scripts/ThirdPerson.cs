ae7ab57b6537fdcc02425d4b5c442da9c64d5f75
Zach <spywoawe@gmail.com>
2020-06-04 10:04:08 +1200

    Controller support TP

--- Oasis/Assets/Scripts/ThirdPerson.cs
+++ Oasis/Assets/Scripts/ThirdPerson.cs
@@ -13,8 +13,8 @@ public class ThirdPerson : MonoBehaviour
     float currentX = 0.0f;
     float currentY = 0.0f;
     public float mouseSensitivity = 3f;
-    //float controllerX = 0.0f;
-    //float controllerY = 0.0f;
+    float controllerX = 0.0f;
+    float controllerY = 0.0f;
 
     private void Start()
     {
@@ -27,23 +27,25 @@ public class ThirdPerson : MonoBehaviour
         currentX += Input.GetAxis("Mouse X");
         currentY += Input.GetAxis("Mouse Y");
         //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        //controllerX += Input.GetAxis("Controller X");
-        //controllerY += Input.GetAxis("Controller Y");
+        controllerX += Input.GetAxis("Controller X");
+        controllerY += Input.GetAxis("Controller Y");
+
 
         Debug.Log(camTransform.rotation.y);
         float yRotation = camTransform.eulerAngles.y;
 
         lookAt.eulerAngles = new Vector3(lookAt.eulerAngles.x, yRotation, lookAt.eulerAngles.z);
         currentY = Mathf.Clamp(currentY, 0f, 50f);
+        controllerY = Mathf.Clamp(controllerY, 0f, 50f);
     }
 
     private void LateUpdate()
     {
         Vector3 dir = new Vector3(0, 0, -distance);
         Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        //Quaternion rotationCon = Quaternion.Euler(controllerY, controllerX, 0);
+        Quaternion rotationCon = Quaternion.Euler(controllerY, controllerX, 0);
         camTransform.position = lookAt.position + rotation * dir;
-        //camTransform.position = lookAt.position + rotationCon * dir;
+        camTransform.position = lookAt.position + rotationCon * dir;
         camTransform.LookAt(lookAt.position);
     }
 }
