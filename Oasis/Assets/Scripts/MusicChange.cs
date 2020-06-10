2e86da88f9999641b4c14c0890c209a623b583e5
Zach <spywoawe@gmail.com>
2020-06-11 09:20:50 +1200

    Ambience added for both land and water + extra sounds added

--- Oasis/Assets/Scripts/MusicChange.cs
+++ Oasis/Assets/Scripts/MusicChange.cs
@@ -6,6 +6,8 @@ public class MusicChange : MonoBehaviour
 {
     public AudioSource land;
     public AudioSource underwater;
+    public AudioSource waterAmbience;
+    public AudioSource landAmbience;
     bool isLandPlaying = true;
     bool isWaterPlaying = false;
 
@@ -21,14 +23,18 @@ public class MusicChange : MonoBehaviour
         if(PlayerMovement.water == true && (isLandPlaying == true && isWaterPlaying == false))
         {
             underwater.Play();
+            waterAmbience.Play();
             land.Stop();
+            landAmbience.Stop();
             isLandPlaying = false;
             isWaterPlaying = true;
         }
         if (PlayerMovement.water == false && (isLandPlaying == false && isWaterPlaying == true))
         {
             underwater.Stop();
+            waterAmbience.Stop();
             land.Play();
+            landAmbience.Play();
             isLandPlaying = true;
             isWaterPlaying = false;
         }
